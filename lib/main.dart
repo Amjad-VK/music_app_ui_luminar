4923912ad6eb212f79b4e9c1221f8dd78fa41479
Amjad-VK <amjadv0@gmail.com>
2023-06-01 09:08:57 +0530

    final

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:music_app_ui/page2.dart';
-import 'package:music_app_ui/page3.dart';
 import 'package:music_app_ui/page3_a.dart';
 
 void main() {
